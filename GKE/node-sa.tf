a29ca2a4bed11cc5231ffde2f3e13efc0dc16a41
mustafasaber36 <mustafasaber36@gmail.com>
2023-03-13 15:41:15 +0200

    last

--- GKE/node-sa.tf
+++ GKE/node-sa.tf
@@ -7,8 +7,8 @@ resource "google_project_iam_member" "storageadmin" {
   role    = "roles/storage.admin"
   member  = "serviceAccount:${google_service_account.node-service-account.email}"
 }
-resource "google_project_iam_member" "containeradmin" {
-  project = "vois-project-379819"
-  role    = "roles/container.admin"
-  member  = "serviceAccount:${google_service_account.node-service-account.email}"
-}
+# resource "google_project_iam_member" "containeradmin" {
+#   project = "vois-project-379819"
+#   role    = "roles/container.admin"
+#   member  = "serviceAccount:${google_service_account.node-service-account.email}"
+# }
