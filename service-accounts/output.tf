05d1d160b4bea8b915f062a59cb03bc65ebc1d00
mustafasaber36 <mustafasaber36@gmail.com>
2023-03-13 22:34:47 +0200

    final

--- service-accounts/output.tf
+++ service-accounts/output.tf
@@ -1,6 +1,7 @@
 output "bucket_service_account_email" {
   value = google_service_account.bucket_service_account.email
 }
+#============bigquery=========================
 output "bigquery_service_account_email" {
   value = google_service_account.bigquery_service_account.email
 }
